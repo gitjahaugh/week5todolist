6acc253f281f46f59fc04ede1e37cf80ed35e577
Jeffrey Haugh <jahaugh@mail.fhsu.edu>
2021-02-28 22:21:57 -0600

    fixed link for sass compiler

--- view/header.php
+++ view/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ToDo List</title>
-    <link rel="stylesheet" ahref="view/css/main.min.css" />
+    <link rel="stylesheet" href="view/css/main.min.css" />
 </head>
 <body>
     <main class="main">
